ec96fd28ce9566d42be7f6179ff3fcdb32f06290
Alan Huxley <alan.huxley@delineo.com>
2024-10-02 15:07:14 +0100

    Added Route 3 post

--- index.js
+++ index.js
@@ -39,7 +39,30 @@ app.get('/update-cobj', async (req, res) => {
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
+app.post('/update-cobj', async (req, res) => {
+    const data = {
+        properties: {
+            "name": req.body.name,
+            "artist": req.body.artist,
+            "label": req.body.label,
+            "description": req.body.description
+        }
+    }
+    const createAlbum = `https://api.hubapi.com/crm/v3/objects/albums`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try { 
+        await axios.post(createAlbum, data, { headers } );
+        console.log("New album created successfully");
+        res.redirect('/');
+    } catch(err) {
+        console.error(err);
+    }
+
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
