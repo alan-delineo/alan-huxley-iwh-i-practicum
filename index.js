10c66e915143d5fd7e509ef5c50d2be5f4bb12f3
Alan Huxley <alan.huxley@delineo.com>
2024-10-02 13:54:46 +0100

    Added updates.pug template, added links between homepage/updates, added test GET route for updates

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
-
 app.get('/', async (req, res) => {
     const albums = 'https://api.hubspot.com/crm/v3/objects/albums?limit=10&properties=name,artist,label,description';
     const headers = {
@@ -23,7 +21,7 @@ app.get('/', async (req, res) => {
     try {
         const resp = await axios.get(albums, { headers });
         const data = resp.data.results;
-        res.render('homepage', { title: 'Albums | HubSpot APIs | Alan Huxley I Practicum', data });      
+        res.render('homepage', { title: 'Albums Custom Objects | Integrating With HubSpot I Practicum', data });      
     } catch (error) {
         console.error(error);
     }
@@ -31,7 +29,20 @@ app.get('/', async (req, res) => {
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+app.get('/update-cobj', async (req, res) => {
+    const albums = 'https://api.hubspot.com/crm/v3/objects/albums?limit=10&properties=name,artist,label,description';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(albums, { headers });
+        const data = resp.data.results;
+        res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum', data });      
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
